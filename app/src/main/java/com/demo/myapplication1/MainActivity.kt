ba9edde9c5b0afbedb505a4e6c5741dedcca74b7
zhangzhaohong <zhangzhaohong@kuaishou.com>
2020-06-14 17:52:56 +0800

    ft:中间提交

--- app/src/main/java/com/demo/myapplication1/MainActivity.kt
+++ app/src/main/java/com/demo/myapplication1/MainActivity.kt
@@ -3,23 +3,68 @@ package com.demo.myapplication1
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.text.clear
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initListener()
 
     }
 
-    fun onClick(view: View) {
+    private fun initListener() {
+        clear.setOnClickListener(this)
+        btn_1.setOnClickListener(this)
+        btn_2.setOnClickListener(this)
+        btn_3.setOnClickListener(this)
+        btn_4.setOnClickListener(this)
+        btn_5.setOnClickListener(this)
+        btn_6.setOnClickListener(this)
+        btn_7.setOnClickListener(this)
+        btn_8.setOnClickListener(this)
+        btn_9.setOnClickListener(this)
+        opr_1.setOnClickListener(this)
+        opr_2.setOnClickListener(this)
+        opr_3.setOnClickListener(this)
+        opr_4.setOnClickListener(this)
+        opr_5.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
         when (view.id) {
-            R.id.download -> {
+            R.id.clear -> {
             }
-            R.id.play -> {
+            R.id.btn_1 -> {
             }
-            R.id.pause -> {
+            R.id.btn_2 -> {
+            }
+            R.id.btn_3 -> {
+            }
+            R.id.btn_4 -> {
+            }
+            R.id.btn_5 -> {
+            }
+            R.id.btn_6 -> {
+            }
+            R.id.btn_7 -> {
+            }
+            R.id.btn_8 -> {
+            }
+            R.id.btn_9 -> {
+            }
+            R.id.opr_1 -> {
+            }
+            R.id.opr_2 -> {
+            }
+            R.id.opr_3 -> {
+            }
+            R.id.opr_4 -> {
+            }
+            R.id.opr_5 -> {
             }
             else -> {
             }
